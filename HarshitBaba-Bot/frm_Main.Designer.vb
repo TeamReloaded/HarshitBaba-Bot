e39703b34cd73f892067b1d83c041858e7d5dd11
Devil7 <dineshthangavel47@gmail.com>
2018-02-25 14:58:03 +0530

    Set Properties

--- HarshitBaba-Bot/frm_Main.Designer.vb
+++ HarshitBaba-Bot/frm_Main.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frm_Main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
